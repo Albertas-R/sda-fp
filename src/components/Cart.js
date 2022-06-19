51be5897fa98423b002411ec76d0190320b3450c
Albertas <albertas.rinkevicius@gmail.com>
2022-06-19 14:11:59 +0300

    06-19 po paskaitos

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 
 import styles from "./Cart.module.css";
 
-function Cart(props) {
+function Cart({ products }) {
   const {
     cart,
     cart_list,
@@ -26,37 +26,52 @@ function Cart(props) {
     navigate("/pay");
   };
 
-  // const cartItems = JSON.parse(localStorage.getItem("cartItem"));
-  // console.log(cartItems);
+  const cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+  console.log(cartItems);
+
+  // 1 variantas - vietoj filter -> reduce
+  // 2 variantas - map productsInCart ir ziuret koks amount
+  const productsInCart = products.filter((el) => {
+    const isProductInCart = cartItems.some((x) => {
+      return el.id === x.id;
+    });
+    return isProductInCart;
+  });
+  // console.log(productsInCart);
 
   return (
     <div className={cart}>
       <ul className={cart_list}>
-        <li className={cart_list_li}>
-          <Link to="/product">
-            <img className={cart_list_li_img} src={props.image} alt="" />
-          </Link>
-          <Link to="/product" className={cart_title}>
-            <p>{props.title}</p>
-          </Link>
-          <p className={cart_price}>{props.price} €</p>
-          <div className={cart_amount}>
-            <div className={cart_amount_nr}>
-              <p>1</p>
-            </div>
-            <div className={cart_amount_btns}>
-              <button className={cart_amount_add}>+</button>
-              <button className={cart_amount_min}>-</button>
-            </div>
-          </div>
-        </li>
+        {productsInCart.map((el) => {
+          // console.log(el);
+          return (
+            <li className={cart_list_li} key={el.id}>
+              <Link to="/product">
+                <img className={cart_list_li_img} src={el.images[0]} alt="" />
+              </Link>
+              <Link to="/product" className={cart_title}>
+                <p>{el.title}</p>
+              </Link>
+              <p className={cart_price}>{el.price} €</p>
+              <div className={cart_amount}>
+                <div className={cart_amount_nr}>
+                  <p>"1"</p>
+                </div>
+                <div className={cart_amount_btns}>
+                  <button className={cart_amount_add}>+</button>
+                  <button className={cart_amount_min}>-</button>
+                </div>
+              </div>
+            </li>
+          );
+        })}
 
-        <li className={cart_list_li}>
+        {/* <li className={cart_list_li}>
           <Link to="/product">
             <img className={cart_list_li_img} src="https://via.placeholder.com/50x40" alt="" />
           </Link>
           <Link to="/product" className={cart_title}>
-            <p>Product 2 Title</p>
+            <p>Product 1 Title</p>
           </Link>
           <p className={cart_price}>50 €</p>
           <div className={cart_amount}>
@@ -68,26 +83,7 @@ function Cart(props) {
               <button className={cart_amount_min}>-</button>
             </div>
           </div>
-        </li>
-
-        <li className={cart_list_li}>
-          <Link to="/product">
-            <img className={cart_list_li_img} src="https://via.placeholder.com/50x40" alt="" />
-          </Link>
-          <Link to="/product" className={cart_title}>
-            <p>Product 3 Title</p>
-          </Link>
-          <p className={cart_price}>75 €</p>
-          <div className={cart_amount}>
-            <div className={cart_amount_nr}>
-              <p>1</p>
-            </div>
-            <div className={cart_amount_btns}>
-              <button className={cart_amount_add}>+</button>
-              <button className={cart_amount_min}>-</button>
-            </div>
-          </div>
-        </li>
+        </li> */}
       </ul>
       <div className={cart_total}>
         Total: <strong>225 €</strong>
