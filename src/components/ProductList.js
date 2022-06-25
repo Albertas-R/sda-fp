45ed13cc122f09adbf547694e0f6b59efceb1da9
Albertas <albertas.rinkevicius@gmail.com>
2022-06-25 13:09:25 +0300

    Search products

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,4 +1,4 @@
-import { React } from "react";
+import { React, useState } from "react";
 
 import Hero from "./Hero";
 import ProductCard from "./ProductCard";
@@ -6,14 +6,33 @@ import ProductCard from "./ProductCard";
 import styles from "./ProductList.module.css";
 
 function ProductList({ products }) {
-  const { product_list } = styles;
+  // console.log(products);
+
+  const { product_list, product_search } = styles;
+
+  const [searchField, setSearchField] = useState("");
+  // console.log(searchField);
+
+  const filteredProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(searchField.toLowerCase())
+  );
 
   return (
     <>
       <Hero />
 
+      <input
+        type="search"
+        placeholder="Search products"
+        onChange={(e) => setSearchField(e.target.value)}
+        className={product_search}
+      />
+
       <ul className={product_list} id="product_list">
-        {products.map((product) => {
+        {/* {products.map((product) => {
+          return <ProductCard product={product} key={product.id} />;
+        })} */}
+        {filteredProducts.map((product) => {
           return <ProductCard product={product} key={product.id} />;
         })}
       </ul>
